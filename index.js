067498cca59712c1d3af0c5466008ecd4deb1b22
Natalie Finnegan <nfinneg2@gmail.com>
2021-04-24 20:44:06 -0600

    added comments to index.js file

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const consoleTable = require("console.table");
 const pwd = require("./pwd");
-// const {
-//   viewAllEmp,
-//   viewAllEmpByDept,
-//   viewAllEmpByRoles,
-// } = require("./main/view");
 
 //create the connection information for the sql database
 const connection = mysql.createConnection({
@@ -23,6 +18,7 @@ const connection = mysql.createConnection({
   database: "companyDB",
 });
 
+//Starter Prompt of Options with a switch/case statement invoking the function corresponding to user input
 const start = () => {
   inquirer
     .prompt([
@@ -76,6 +72,7 @@ const start = () => {
     });
 };
 
+//View ALL Employees
 const viewAllEmp = () => {
   connection.query(
     `SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, CONCAT(m.first_name,' ',m.last_name) AS manager FROM employees
@@ -91,6 +88,7 @@ const viewAllEmp = () => {
   );
 };
 
+//View ALL Employees by Department
 const viewAllEmpByDept = () => {
   connection.query(
     "SELECT employees.id, employees.first_name, employees.last_name, department.dept_name AS department FROM employees JOIN department USING (id)",
@@ -103,6 +101,7 @@ const viewAllEmpByDept = () => {
   );
 };
 
+//View ALL Employees by Role
 const viewAllEmpByRoles = () => {
   connection.query(
     "SELECT employees.id, employees.first_name, employees.last_name, roles.title AS role, roles.salary FROM employees JOIN roles ON employees.role_id = roles.id",
@@ -115,14 +114,18 @@ const viewAllEmpByRoles = () => {
   );
 };
 
+//Updates Existing Employee
 const updateEmpRole = () => {
+  //declare empty array to put each employee in once the query runs to be used in the choices list for the inquirer prompt
   const empList = [];
+  //declare empty array to put each role in oce the query runs to be used in the choices list for the inquirer prompt
   const roleList = [];
   connection.query(
     "SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS full_name, roles.title AS role FROM employees LEFT JOIN roles ON employees.role_id = roles.id",
     (err, res) => {
       if (err) throw err;
       console.table(res);
+      //for each element in the array, push the employee's full name, which was created as an alias (concatenated by above in the query statement)
       res.forEach((employee) => {
         empList.push(employee.full_name);
       });
@@ -132,6 +135,7 @@ const updateEmpRole = () => {
         (err, res) => {
           if (err) throw err;
           // console.log(res);
+          //deconstruct the id and title of each role in the database and push it into the empty array
           res.forEach(({ id, title }, i) => {
             i++;
             roleList.push(`${id} ${title}`);
@@ -155,15 +159,21 @@ const updateEmpRole = () => {
           },
         ])
         .then((answers) => {
-          //console.log(answers.empChoice);
+          //split off the last name from the empList using split on the 1st index, so we can make sure we are updating the role to the
+          //correct corresponding employee, vs the entire employee list
           let lastName = answers.empChoice.split(" ")[1];
+
           let updatedRole;
+          //filter through each element in the role list array, if the it matches the user input execute the code block in the if statement
           roleList.filter((rle) => {
             if (rle === answers.role) {
+              //split off the role ID that was pushed into the roleList array so that we can update the employee,
+              //using their last name, to their new updated role
               updatedRole = rle.split(" ")[0];
               return updatedRole;
             }
           });
+          //updating employee role in the database
           connection.query(
             "UPDATE employees SET ? WHERE ?",
             [
@@ -185,6 +195,7 @@ const updateEmpRole = () => {
   );
 };
 
+//add a New Role
 const addRole = () => {
   inquirer
     .prompt([
@@ -215,6 +226,7 @@ const addRole = () => {
     });
 };
 
+//adding new department
 const addDept = () => {
   inquirer
     .prompt([
@@ -239,20 +251,27 @@ const addDept = () => {
     });
 };
 
+//adding new employee
 const addEmp = () => {
   let mgrQuery = "SELECT * FROM employees";
+  //empty array declared to put every employee/id into once query is run
   const mgrList = [];
   connection.query(mgrQuery, (err, res) => {
     if (err) throw err;
+
+    //for each element in the mgrQuery, take id, first and last name and push it into the empty mgrList
+    //array to be used in the choices in the inquirer prompt
     res.forEach(({ id, first_name, last_name }, i) => {
       i++;
       mgrList.push(`${id} ${first_name} ${last_name}`);
     });
     let roleQuery = "SELECT roles.id, roles.title FROM roles";
+    //empty array declared to put all roles into from database roleQuery
     const allRoles = [];
     connection.query(roleQuery, (err, res) => {
       if (err) throw err;
-      // console.log(res);
+      //for each element in roleQuery, take id & title and push it into the empty allRoles array
+      //to be displayed in choices for inquirer prompt
       res.forEach(({ id, title }, i) => {
         i++;
         allRoles.push(`${id} ${title}`);
@@ -286,6 +305,8 @@ const addEmp = () => {
       ])
       .then((answers) => {
         let newEmpID;
+        //filter through the array and find the element that matches user input, split off the ID number
+        //to ensure we get the ID number of that employee and input it into the managers ID column in the DB
         mgrList.filter((emp) => {
           if (emp === answers.empMgr) {
             newEmpID = emp.split(" ")[0];
@@ -294,6 +315,7 @@ const addEmp = () => {
         });
         // console.log("Line 262", newEmpID);
         let newEmpRole;
+        //filter through allRoles array to find element that matches user input, split off the ID number to ensure we are adding the right role id to the DB
         allRoles.filter((rle) => {
           if (rle === answers.empRole) {
             newEmpRole = rle.split(" ")[0];
@@ -321,6 +343,7 @@ const addEmp = () => {
   });
 };
 
+//Initialize App
 connection.connect((err) => {
   if (err) throw err;
   console.log(`connected as id ${connection.threadId}\n`);
